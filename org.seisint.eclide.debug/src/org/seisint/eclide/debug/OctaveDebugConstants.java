821f162b6041a7f828ee99545c43eaf85412d943
Gordon Smith <gordonjsmith@gmail.com>
2011-09-01 18:27:43 +0100

    WIP:  Remove DLTK

--- org.seisint.eclide.debug/src/org/seisint/eclide/debug/OctaveDebugConstants.java
+++
@@ -1,9 +0,0 @@
-package org.seisint.eclide.debug;
-
-public class OctaveDebugConstants {
-
-	public static final String DEBUG_MODEL_ID = "org.seisint.eclide.debug.octaveModel";
-
-	public static final String DEBUGGING_ENGINE_ID_KEY = "debugging_engine_id";
-
-}
