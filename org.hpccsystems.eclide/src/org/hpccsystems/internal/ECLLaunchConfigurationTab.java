996f291b889d44979396efc2e7a1dc9eacc760c5
Gordon Smith <gordonjsmith@gmail.com>
2011-10-11 07:26:48 +0100

    WIP:  Missing file

---
+++ org.hpccsystems.eclide/src/org/hpccsystems/internal/ECLLaunchConfigurationTab.java
@@ -0,0 +1,44 @@
+package org.hpccsystems.internal;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.ui.progress.WorkbenchJob;
+
+public abstract class ECLLaunchConfigurationTab extends AbstractLaunchConfigurationTab {
+	private Job fRereshJob;	
+
+	private Job getUpdateJob() {
+		if (fRereshJob == null) {
+			fRereshJob = createUpdateJob();
+			fRereshJob.setSystem(true);
+		}
+		return fRereshJob;
+	}
+	
+	protected void scheduleUpdateJob() {
+		Job job = getUpdateJob();
+		job.cancel(); // cancel existing job
+		job.schedule(getUpdateJobDelay());
+	}
+	
+	protected Job createUpdateJob() {
+		return  new WorkbenchJob(getControl().getDisplay(), "Update LCD") { //$NON-NLS-1$
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				if (!getControl().isDisposed()) {
+					updateLaunchConfigurationDialog();
+				}
+				return Status.OK_STATUS;
+			}
+			public boolean shouldRun() {
+				return !getControl().isDisposed();
+			}
+		};
+	}
+	
+	protected long getUpdateJobDelay() {
+		return 200;
+	}	
+}
