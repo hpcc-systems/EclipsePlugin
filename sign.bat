d240a5411f8729737d10caf8fb40bb6a74a9a5b4
Gordon Smith <gordon.smith@lexisnexis.com>
2013-01-08 09:30:38 +0000

    Issue_80 Add Eclipse Juno Support
    Fixes Issue_80
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

---
+++ sign.bat
@@ -0,0 +1,17 @@
+set JARSIGNER=c:\Program Files\Java\jdk1.7.0_10\bin\jarsigner.exe
+set KEYSTORE=..\sign\hpcc_code_signing.pfx
+set TSA=https://timestamp.geotrust.com/tsa
+set STOREPASS=%1
+
+set SIGN_FILTER=.\org.hpccsystems.updatesite\features\*.jar
+CALL :DOSIGN 
+
+set SIGN_FILTER=.\org.hpccsystems.updatesite\plugins\*.jar
+CALL :DOSIGN 
+
+GOTO :EOF
+
+:DOSIGN
+for /f %%a IN ('dir /b /s "%SIGN_FILTER%"') do call "%JARSIGNER%" -storetype pkcs12 --digestalg SHA1 -sigalg MD5withRSA -keystore "%KEYSTORE%" storepass "%STOREPASS%" -tsa "%TSA%" -verbose "%%a" 1
+GOTO :EOF
+
